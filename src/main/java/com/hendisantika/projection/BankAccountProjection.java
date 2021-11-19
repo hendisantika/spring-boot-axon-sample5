0b5f9a53a50243aa693f02c6c5cde0f4d2137248
Hendi Santika <hendisantika@gmail.com>
2021-11-20 06:42:32 +0700

    #20 - Add BankAccountProjection

---
+++ src/main/java/com/hendisantika/projection/BankAccountProjection.java
@@ -0,0 +1,25 @@
+package com.hendisantika.projection;
+
+import com.hendisantika.repository.BankAccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/21
+ * Time: 06.42
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BankAccountProjection {
+
+    private final BankAccountRepository repository;
+    private final QueryUpdateEmitter updateEmitter;
+}
