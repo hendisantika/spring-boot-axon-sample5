92b6052592469c07e14acf117f466dfca9b966fe
Hendi Santika <hendisantika@gmail.com>
2021-11-20 06:33:40 +0700

    #4 - Add BankAccount Entity

---
+++ src/main/java/com/hendisantika/entity/BankAccount.java
@@ -0,0 +1,30 @@
+package com.hendisantika.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/21
+ * Time: 06.33
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BankAccount {
+    @Id
+    private UUID id;
+    private String owner;
+    private BigDecimal balance;
+}
