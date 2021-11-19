d1f990548f249d76cb1688934ed9581370f4210d
Hendi Santika <hendisantika@gmail.com>
2021-11-20 06:55:53 +0700

    #46 - Add AccountQueryController

---
+++ src/main/java/com/hendisantika/controller/AccountQueryController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.AccountQueryService;
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-axon-sample5
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/11/21
+ * Time: 06.55
+ */
+@RestController
+@RequestMapping(value = "/accounts")
+@Api(value = "Bank Account Queries", description = "Bank Account Query Events API")
+@AllArgsConstructor
+public class AccountQueryController {
+
+    private final AccountQueryService accountQueryService;
+
+}
