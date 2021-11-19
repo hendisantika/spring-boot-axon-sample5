a43675433088d41684504b99ef1976439ad0a7a9
Hendi Santika <hendisantika@gmail.com>
2021-11-20 06:46:48 +0700

    #29 - Add handleAccountNotFound in ExceptionHandlers

--- src/main/java/com/hendisantika/exception/ExceptionHandlers.java
+++ src/main/java/com/hendisantika/exception/ExceptionHandlers.java
@@ -1,8 +1,12 @@
 package com.hendisantika.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +24,12 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler {
     private static final String REQUESTED_ACCOUNT_NOT_FOUND = "Requested account not found";
     private static final String INSUFFICIENT_BALANCE_EXCEPTION = "Debit operation aborted : Insufficient Balance";
 
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<Object> handleAccountNotFound(AccountNotFoundException ex) {
+        log.error(REQUESTED_ACCOUNT_NOT_FOUND);
+
+        return buildResponseEntity(
+                new ResponseError(NOT_FOUND, REQUESTED_ACCOUNT_NOT_FOUND)
+        );
+    }
 }
