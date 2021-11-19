9005439c69d49f134b33ba6a5426af52d3706326
Hendi Santika <hendisantika@gmail.com>
2021-11-20 06:29:24 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/SpringBootAxonSample5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAxonSample5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
